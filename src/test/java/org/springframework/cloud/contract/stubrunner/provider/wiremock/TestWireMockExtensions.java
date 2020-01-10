8bb2d746dc23714ffa7895bdb2eb4f9135464a1f
Anton Hlinisty <v-ahlinisty@ebsco.com>
2020-01-10 17:37:29 +0300

    Move extension to java

---
+++ src/test/java/org/springframework/cloud/contract/stubrunner/provider/wiremock/TestWireMockExtensions.java
@@ -0,0 +1,32 @@
+package org.springframework.cloud.contract.stubrunner.provider.wiremock;
+
+import com.github.tomakehurst.wiremock.extension.Extension;
+import org.springframework.cloud.contract.verifier.dsl.wiremock.DefaultResponseTransformer;
+import org.springframework.cloud.contract.verifier.dsl.wiremock.WireMockExtensions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Extension that registers the default transformer and the custom one
+ */
+class TestWireMockExtensions implements WireMockExtensions {
+
+    @Override
+    public List<Extension> extensions() {
+        return List.of(
+                new DefaultResponseTransformer(),
+                new CustomExtension()
+        );
+    }
+
+    class CustomExtension implements Extension {
+
+        @Override
+        public String getName() {
+            return "foo-transformer";
+        }
+    }
+}
+
+
